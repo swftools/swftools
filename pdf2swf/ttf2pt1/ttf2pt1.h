710c9cd13c2d123aca6720b92a7ef5051daceeb2
kramm <kramm>
2003-07-05 17:38:34 +0000

    cplusplus handling.

--- pdf2swf/ttf2pt1/ttf2pt1.h
+++ pdf2swf/ttf2pt1/ttf2pt1.h
@@ -1,8 +1,15 @@
 #ifndef __ttf2pt1_included__
+#define __ttf2pt1_included__
+#ifdef __cplusplus
+extern "C" {
+#endif
 int
 ttf2pt1_main(
      int argc,
      char **argv
 );
+#ifdef __cplusplus
+}
+#endif
 #endif
 
