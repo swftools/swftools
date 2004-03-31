662140a5161907a74d7b0af06d4c65d056dc5b7a
kramm <kramm>
2004-03-31 13:39:44 +0000

    added progress field

--- avi2swf/videoreader.h
+++ avi2swf/videoreader.h
@@ -20,6 +20,9 @@ typedef struct _videoreader_t
     int channels;
     int rate;
 
+    /* progress */
+    int frame;
+
     void (*setparameter) (struct _videoreader_t*, char*name, char*value);
     int (*getsamples) (struct _videoreader_t*, void*buffer, int num);
     int (*getimage) (struct _videoreader_t*, void*buffer); // buffer must hold width*height*4 bytes
