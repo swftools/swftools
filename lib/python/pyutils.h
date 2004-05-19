ab2e2a03bb1d26ebaed40ed76b522151cbdca633
kramm <kramm>
2004-05-19 07:48:23 +0000

    added PY_ERROR and PY_TYPE.

--- lib/python/pyutils.h
+++ lib/python/pyutils.h
@@ -16,4 +16,7 @@ PyMethodDef* addMethods(PyMethodDef*obj1, PyMethodDef*obj2);
 
 #define PY_CHECK_TYPE(o,c) ((o)->ob_type == (c))
 #define PY_ASSERT_TYPE(o,c) {if((o)->ob_type != (c)) {printf("Invalid type: %08x(%d)", (int)(o), (o)->ob_refcnt);exit(1);}}
+	
+#define PY_ERROR(s,args...) (PyErr_SetString(PyExc_Exception, setError(s, ## args)),NULL)
+#define PY_TYPE(o) ((o)->ob_type->tp_name)
 #endif
