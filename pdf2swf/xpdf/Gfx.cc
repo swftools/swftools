5345c480ef41da2b734dcc24a1db14442d274d87
kramm <kramm>
2004-07-05 10:59:12 +0000

    * removed explicit use of cropbox for cropping * pass cropbox to OutputDev.

--- pdf2swf/xpdf/Gfx.cc
+++ pdf2swf/xpdf/Gfx.cc
@@ -397,7 +397,7 @@ Gfx::Gfx(XRef *xrefA, OutputDev *outA, int pageNum, Dict *resDict, double dpi,
   fontChanged = gFalse;
   clip = clipNone;
   ignoreUndef = 0;
-  out->startPage(pageNum, state);
+  out->startPage(pageNum, state, cropBox->x1,cropBox->y1,cropBox->x2,cropBox->y2);
   out->setDefaultCTM(state->getCTM());
   out->updateAll(state);
   for (i = 0; i < 6; ++i) {
@@ -405,7 +405,7 @@ Gfx::Gfx(XRef *xrefA, OutputDev *outA, int pageNum, Dict *resDict, double dpi,
   }
 
   // set crop box
-  if (crop) {
+  /*if (crop) {
     state->moveTo(cropBox->x1, cropBox->y1);
     state->lineTo(cropBox->x2, cropBox->y1);
     state->lineTo(cropBox->x2, cropBox->y2);
@@ -414,7 +414,7 @@ Gfx::Gfx(XRef *xrefA, OutputDev *outA, int pageNum, Dict *resDict, double dpi,
     state->clip();
     out->clip(state);
     state->clearPath();
-  }
+  }*/
 }
 
 Gfx::Gfx(XRef *xrefA, OutputDev *outA, Dict *resDict,
