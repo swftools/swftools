89de887d9a11f7b8a42dcf68c13a1ffd384ced21
kramm <kramm>
2004-11-22 19:18:46 +0000

    fixed bug in SWFOutputDev::getDimensions().

--- pdf2swf/SWFOutputDev.cc
+++ pdf2swf/SWFOutputDev.cc
@@ -355,7 +355,7 @@ void SWFOutputDev::setClip(int x1,int y1,int x2,int y2)
 }
 void SWFOutputDev::getDimensions(int*x1,int*y1,int*x2,int*y2)
 {
-    return swfoutput_getdimensions(&output, x1,y2,x2,y2);
+    return swfoutput_getdimensions(&output, x1,y1,x2,y2);
 }
 
 static char*getFontID(GfxFont*font)
