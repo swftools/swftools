608c6b4c741f4f26258e7158c922b1215b855f25
kramm <kramm>
2006-06-05 07:52:20 +0000

    added __cplusplus handling

--- lib/mem.h
+++ lib/mem.h
@@ -1,6 +1,10 @@
 #ifndef __mem_h__
 #define __mem_h__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define ALLOC_ARRAY(type, num) (((type)*)rfxalloc(sizeof(type)*(num)))
 void* rfx_alloc(int size);
 void* rfx_calloc(int size);
@@ -10,4 +14,9 @@ void rfx_free(void*data);
 long rfx_memory_used();
 char* rfx_memory_used_str();
 #endif
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif //__mem_h__
