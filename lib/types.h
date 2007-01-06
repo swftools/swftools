cd3c954f7aa80b7388abe13e7ae9b924a1bb4953
kramm <kramm>
2007-01-06 22:27:21 +0000

    added 64 bit types

--- lib/types.h
+++ lib/types.h
@@ -30,11 +30,13 @@
 
 // SWF Types
 
-typedef         unsigned long   U32;
-typedef         signed long     S32;
-typedef         unsigned short  U16;
-typedef         signed short    S16;
-typedef         unsigned char   U8;
-typedef         signed char     S8;
+typedef         unsigned long long  U64;
+typedef         signed long long    S64;
+typedef         unsigned long       U32;
+typedef         signed long         S32;
+typedef         unsigned short      U16;
+typedef         signed short        S16;
+typedef         unsigned char       U8;
+typedef         signed char         S8;
 
 #endif
