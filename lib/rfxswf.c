14bda0e9c2ceecb14bce0710a92e033e5727a94b
kramm <kramm>
2007-01-13 17:25:04 +0000

    added writing of flash 8 setattributes tag

--- lib/rfxswf.c
+++ lib/rfxswf.c
@@ -1236,6 +1236,15 @@ int  swf_WriteSWF2(writer_t*writer, SWF * swf)     // Writes SWF to file, return
 
   if(writer) writer_lastpos = writer->pos;
 
+  if(swf->fileVersion >= 8) {
+      if ((swf->firstTag && swf->firstTag->id != ST_FILEATTRIBUTES) &&
+	  (!swf->firstTag->next || swf->firstTag->next->id != ST_FILEATTRIBUTES)) 
+      {
+	  U32 flags = 0; // | 128 = usenetwork, | 8 = hasmetadata
+	  swf_SetU32(swf_InsertTagBefore(swf, swf->firstTag,ST_FILEATTRIBUTES),flags);
+      }
+  }
+
   // Insert REFLEX Tag
 
 #ifdef INSERT_RFX_TAG
