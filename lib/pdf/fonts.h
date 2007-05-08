c669ade1e8806929ffb32d700a2f3ddd25f5f68c
kramm <kramm>
2007-05-08 12:35:59 +0000

    pdf standard fonts

---
+++ lib/pdf/fonts.h
@@ -0,0 +1,41 @@
+#ifndef __fonts_h__
+#define __fonts_h__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern char* d050000l_afm;
+extern char* d050000l_pfb;
+extern char* n019003l_afm;
+extern char* n019003l_pfb;
+extern char* n019004l_afm;
+extern char* n019004l_pfb;
+extern char* n019023l_afm;
+extern char* n019023l_pfb;
+extern char* n019024l_afm;
+extern char* n019024l_pfb;
+extern char* n021003l_afm;
+extern char* n021003l_pfb;
+extern char* n021004l_afm;
+extern char* n021004l_pfb;
+extern char* n021023l_afm;
+extern char* n021023l_pfb;
+extern char* n021024l_afm;
+extern char* n021024l_pfb;
+extern char* n022003l_afm;
+extern char* n022003l_pfb;
+extern char* n022004l_afm;
+extern char* n022004l_pfb;
+extern char* n022023l_afm;
+extern char* n022023l_pfb;
+extern char* n022024l_afm;
+extern char* n022024l_pfb;
+extern char* s050000l_afm;
+extern char* s050000l_pfb;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
