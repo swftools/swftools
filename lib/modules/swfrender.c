2e9e3fa8fc322ec9fb73bcf7a732b6a4ce42749c
kramm <kramm>
2008-03-26 12:59:13 +0000

    added support for gradient rendering

--- lib/modules/swfrender.c
+++ lib/modules/swfrender.c
@@ -23,9 +23,6 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 #include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "../rfxswf.h"
 
 /* one bit flag: */
 #define clip_type 0
