20e80a1d00c42ea569ebe2c6fb7bea6737f8f743
kramm <kramm>
2008-04-01 22:39:28 +0000

    moved from ../swf

---
+++ lib/readers/image.h
@@ -0,0 +1,17 @@
+#ifndef __image_h__
+#define __image_h__
+
+#include "../gfxdevice.h"
+#include "../gfxsource.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+gfxsource_t*gfxsource_image_create();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
