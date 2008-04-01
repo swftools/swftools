cd3802e31c21e435324ea122f4af7bf0917b4923
kramm <kramm>
2008-04-01 22:41:11 +0000

    fixed #ifdef

--- lib/readers/swf.h
+++ lib/readers/swf.h
@@ -1,9 +1,8 @@
-/* swfoutput.h
-   Header file for swfoutput.cc (and swfoutput_x11.cc).
+/* swf.h
 
    Part of the swftools package.
 
-   Copyright (c) 2001,2002,2003 Matthias Kramm <kramm@quiss.org> 
+   Copyright (c) 2008 Matthias Kramm <kramm@quiss.org> 
  
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -19,8 +18,8 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
-#ifndef __pdfsource_h__
-#define __pdfsource_h__
+#ifndef __swfsource_h__
+#define __swfsource_h__
 
 #include "../gfxsource.h"
 
