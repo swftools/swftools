57daf2c90dc409a2aa7030ce6ce996707e99967b
kramm <kramm>
2008-05-24 15:17:31 +0000

    fixed bug in swf_SetFixed(8)

--- lib/rfxswf.c
+++ lib/rfxswf.c
@@ -261,7 +261,7 @@ double swf_GetFixed(TAG * t)
 }
 void swf_SetFixed(TAG * t, double f)
 {
-  U16 fr = (U16)(f-(int)f)*65536;
+  U16 fr = (U16)((f-(int)f)*65536);
   swf_SetU16(t, fr);
   swf_SetU16(t, (U16)f - (f<0 && fr!=0));
 }
@@ -273,7 +273,7 @@ float swf_GetFixed8(TAG * t)
 }
 void swf_SetFixed8(TAG * t, float f)
 {
-  U8 fr = (U8)(f-(int)f)*256;
+  U8 fr = (U8)((f-(int)f)*256);
   swf_SetU8(t, fr);
   swf_SetU8(t, (U8)f - (f<0 && fr!=0));
 }
