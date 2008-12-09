b1d1dd968034b5c2e87caa49fd255ff836ea55af
kramm <kramm>
2008-12-09 13:39:47 +0000

    added break statement

--- lib/as3/opcodes.c
+++ lib/as3/opcodes.c
@@ -1,26 +1,3 @@
-/* opcodes.c
-
-   Routines for handling Flash2 AVM2 ABC Actionscript
-
-   Extension module for the rfxswf library.
-   Part of the swftools package.
-
-   Copyright (c) 2008 Matthias Kramm <kramm@quiss.org>
- 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
-
 #include "opcodes.h"
 code_t* abc_add(code_t*prev)
 {
@@ -950,5 +927,10 @@ code_t* abc_typeof(code_t*prev)
 code_t* abc_urshift(code_t*prev)
 {
     code_t*self = add_opcode(prev, 0xa7);
+    return self;
+}
+code_t* abc___break__(code_t*prev)
+{
+    code_t*self = add_opcode(prev, 0xff);
     return self;
 }
--- lib/as3/opcodes.h
+++ lib/as3/opcodes.h
@@ -1,26 +1,3 @@
-/* opcodes.h
-
-   Routines for handling Flash2 AVM2 ABC Actionscript
-
-   Extension module for the rfxswf library.
-   Part of the swftools package.
-
-   Copyright (c) 2008 Matthias Kramm <kramm@quiss.org>
- 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
-
 #ifndef __opcodes_h__
 #define __opcodes_h__
 #include "abc.h"
@@ -350,4 +327,6 @@ code_t* abc_typeof(code_t*prev);
 #define typeof(method) (method->code = abc_typeof(method->code))
 code_t* abc_urshift(code_t*prev);
 #define urshift(method) (method->code = abc_urshift(method->code))
+code_t* abc___break__(code_t*prev);
+#define __break__(method) (method->code = abc___break__(method->code))
 #endif
