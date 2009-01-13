80fabd0e494d8049b3f3f793ad0444c0ea38bdb8
kramm <kramm>
2009-01-13 22:28:56 +0000

    added a few missing header files

--- lib/gocr/job.c
+++ lib/gocr/job.c
@@ -18,6 +18,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
  see README for email address */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <memory.h>
 #include "pgm2asc.h"
 #include "gocr.h"
 
