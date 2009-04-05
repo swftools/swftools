508e16030c78d6d0083317ec96f2588b503709d3
Matthias Kramm <kramm@quiss.org>
2009-04-05 18:29:22 +0200

    added copyright headerse

--- wx/images.py
+++ wx/images.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python
+#
+# images.py
+#
+# graphical user interface for pdf2swf: image data
+#
+# Part of the swftools package.
+# 
+# Copyright (c) 2008,2009 Matthias Kramm <kramm@quiss.org> 
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+
 raw_width=100
 raw_height=100
 raw_data="""\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xfe\xfe\xfe\xfe\xfe\xff\xff\xff\xfe\xfe\xfe\xff\xff\xff\xfe\xfe\xfe\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xfe\xfe\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfa\xfa\xfa\xff\xff\xffQQQ\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"""+\
--- wx/pdf2swf.py
+++ wx/pdf2swf.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: ISO-8859-15 -*-
+#
+# pdf2swf.py
+# graphical user interface for pdf2swf
+#
+# Part of the swftools package.
+# 
+# Copyright (c) 2008,2009 Matthias Kramm <kramm@quiss.org> 
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
 
 import sys
 import wx
@@ -1041,24 +1063,6 @@ class Pdf2swfFrame(wx.Frame):
             #hsplit.SplitVertically(p1,p2, sashPosition=64)
             hsplit.SplitVertically(self.pagelist, self.onepage, sashPosition=ICON_SIZE*3/2)
             hsplit.SetMinimumPaneSize(10)
-
-#class TestWindow(wx.SplitterWindow):
-#    def __init__(self,parent):
-#        wx.SplitterWindow.__init__(self, parent, -1)
-#        panel = wx.Panel(self, -1)
-#        self.txt = wx.StaticText(panel, -1, "bla bla bla", (100,10), (160,-1), wx.ALIGN_CENTER|wx.ALIGN_CENTER_VERTICAL)
-#        self.SetMinSize((320,200))
-#        self.txt.SetForegroundColour("blue")
-#        self.txt.SetBackgroundColour("green")
-#        self.Fit()
-#        self.Initialize(panel)
-#
-#class TestFrame(wx.Frame):
-#    def __init__(self,parent):
-#        wx.Frame.__init__(self, None, -1)
-#        panel = wx.Panel(self, -1)
-#        window = TestWindow(panel)
-#        window.Show()
 
 class MyApp(wx.App):
     def __init__(self):
