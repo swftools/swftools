4b6d6b957d4c4f62129dfcf8b799692e9af70bad
Matthias Kramm <kramm@quiss.org>
2011-09-20 12:51:08 -0700

    bugfix in rescale filter

--- lib/filters/rescale_images.c
+++ lib/filters/rescale_images.c
@@ -76,7 +76,7 @@ void rescale_images_fillbitmap(gfxfilter_t*dev, gfxline_t*line, gfximage_t*img, 
         m.m01 = (m.m01 * img->width) / new_width;
         m.m10 = (m.m10 * img->height) / new_height;
         m.m11 = (m.m11 * img->height) / new_height;
-        out->fillbitmap(out, line, img, &m, cxform);
+        out->fillbitmap(out, line, new_image, &m, cxform);
         gfximage_free(new_image);
     } else {
         out->fillbitmap(out, line, img, matrix, cxform);
