65b6e918c8b5f6ab65f4fe13a2107e6167a58475
Matthias Kramm <kramm@quiss.org>
2012-07-06 09:38:44 -0700

    added missing file

---
+++ lib/pdf/popplerincludes.h
@@ -0,0 +1,29 @@
+#ifndef __popplerincludes_h__
+#define __popplerincludes_h__
+
+#include "../../config.h"
+#include "../os.h"
+
+//#ifndef HAVE_POPPLER
+//  #define GFXSplashOutputDev SplashOutputDev
+//#endif
+
+#include <poppler-config.h>
+#include <goo/GooString.h>
+#include <goo/gfile.h>
+#include <Object.h>
+#include <Stream.h>
+#include <Array.h>
+#include <Dict.h>
+#include <XRef.h>
+#include <Catalog.h>
+#include <Page.h>
+#include <PDFDoc.h>
+#include <Error.h>
+#include <Link.h>
+#include <OutputDev.h>
+#include <GfxFont.h>
+#include <GfxState.h>
+#include <GlobalParams.h>
+
+#endif
