f16a108148e867fd0ee1e3921089402d7aca527e
Yoshihiro Yamazaki <yoya@awm.jp>
2020-01-23 22:09:24 +0900

    src/swfc-history.h include guard malfunction fixing to typo __HISTORY_Y => __HISTORY_H (This is not harmful to actual operation)

--- src/swfc-history.h
+++ src/swfc-history.h
@@ -20,7 +20,7 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 #ifndef __HISTORY_H
-#define __HISTORY_Y
+#define __HISTORY_H
 
 #include "../lib/types.h"
 #include "../lib/rfxswf.h"
